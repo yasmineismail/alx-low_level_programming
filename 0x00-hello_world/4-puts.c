3b83c179b77361f05d833dd16d726650c251f15d
Yasmine ismail <yasmineiismail@gmail.com>
2023-03-17 01:33:30 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * The main function prints a message to the console.
+ *
+ * @return 0 indicating successful program execution.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
