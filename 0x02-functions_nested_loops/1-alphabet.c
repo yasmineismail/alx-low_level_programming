0b56d8160d077312be44f7f7df32b4f74d872952
Yasmine ismail <yasmineiismail@gmail.com>
2023-03-21 03:05:44 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+void print_alphabet(void){
+    int i = 'a';
+    while(i <= 'z' ){
+    _putchar(i);
+        i ++; 
+    }
+    _putchar('\n');
+}
