8c53e627f1e6cb8c65c30fcc00dafc7ce9bf0125
Yasmine ismail <yasmineiismail@gmail.com>
2023-03-27 12:00:40 -0700

    random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,32 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
  * main - Check code 
  * Return: 0
  */
-#define ARRAY_SIZE 100
-#define MAX_PASS_VALUE 78
-#define REQUIRED_SUM 2772
+int main(void)
+{
+	int i, sum, n;
+	int pass[100];
 
-int main(void) {
-    int i, sum, n;
-    int pass[ARRAY_SIZE];
+	sum = 0;
 
-    sum = 0;
+	srand(time(NULL));
 
-    srand(time(NULL));
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 
-    for (i = 0; i < ARRAY_SIZE; i++) {
-        pass[i] = rand() % MAX_PASS_VALUE;
-        sum += pass[i] + '0';
-        putchar(pass[i] + '0');
-        if (REQUIRED_SUM - sum < MAX_PASS_VALUE) {
-            n = REQUIRED_SUM - sum - '0';
-            sum += n;
-            putchar(n + '0');
-            break;
-        }
-    }
-
-    return 0;
+	return (0);
 }
