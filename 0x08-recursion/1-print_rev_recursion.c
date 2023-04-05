afd4d469d967bbd62a36ad587e7e45fe63cf6b9f
Yasmine ismail <yasmineiismail@gmail.com>
2023-04-05 11:19:19 -0700

    string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
