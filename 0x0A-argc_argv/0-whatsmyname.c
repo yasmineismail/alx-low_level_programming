45a718ac5156fbde5e5305cea00ff2fd121b459c
Yasmine ismail <yasmineiismail@gmail.com>
2023-04-14 03:57:33 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - prints its name, followed by a new line
+  * @argc: The number of arguments
+  * @argv: An array of pointers to the arguments
+  * Return: Always 0
+  */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
