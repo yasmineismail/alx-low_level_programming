54e0284eac7c259de7db1ff91cb47f1f8b1a9a9d
Yasmine ismail <yasmineiismail@gmail.com>
2023-04-17 13:42:49 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * free_dog - frees dogs.
+  * @d: dog struct
+  * Return: void
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
