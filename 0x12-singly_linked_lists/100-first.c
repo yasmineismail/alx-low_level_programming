79121698cf1f7d726256a802de6d596776d9ea46
Yasmine ismail <yasmineiismail@gmail.com>
2023-04-27 11:06:10 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+void startup_print(void) __attribute__ ((constructor));
 
 /**
  * startup_print - prints a startup message.
+ * Return: void
  */
 void startup_print(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
